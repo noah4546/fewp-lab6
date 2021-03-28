9464c35bd102d42f1891ce62d386b25be5ff8a76
Noah Tomkins <1tomkinsnoa@gmail.com>
2021-03-28 14:31:23 -0400

    Added Delete

--- src/App.js
+++ src/App.js
@@ -8,22 +8,24 @@ import DeleteIcon from "./images/delete.svg";
 class GradeForm extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            courseName: "",
-            grade: ""
-        };
+            grade: {id: -1, courseName: "", grade: ""},
+        }; 
     }
 
     handleButton() {
-        this.props.onChange(this.props.editing ? 'edit' : 'add', this.state.courseName, this.state.grade);
+        this.props.onChange(this.props.editing ? 'edit' : 'add', this.state.grade);
     }
 
     handleCourseNameChange(event) {
-        this.setState({courseName: event.target.value});
+        let grade = {id: -1, courseName: event.target.value, grade: this.state.grade.grade};
+        this.setState({grade: grade});
     }
 
     handleGradeChange(event) {
-        this.setState({grade: event.target.value});
+        let grade = {id: -1, courseName: this.state.grade.courseName, grade: event.target.value};
+        this.setState({grade: grade});
     }
 
     render() {
@@ -36,7 +38,7 @@ class GradeForm extends React.Component {
                             type="text" 
                             className="form-control" 
                             placeholder="Course name" 
-                            value={this.state.courseName}
+                            value={this.state.grade.courseName}
                             onChange={this.handleCourseNameChange.bind(this)}
                         />
                     </div>
@@ -47,7 +49,7 @@ class GradeForm extends React.Component {
                             placeholder="Grade" 
                             min="0" 
                             max="100" 
-                            value={this.state.grade}
+                            value={this.state.grade.grade}
                             onChange={this.handleGradeChange.bind(this)}
                         />
                     </div>
@@ -68,13 +70,33 @@ class GradesList extends React.Component {
         super(props);
     }
 
+    handleEdit(id) {
+        this.props.onEdit(id);
+    }
+
+    handleDelete(id) {
+        this.props.onDelete(id);
+    }
+
     render() {
         let grades = this.props.grades.map(grade => (
             <tr key={grade.id}>
                 <td>{grade.courseName}</td>
                 <td>{grade.grade}</td>
-                <td><Button variant="light"><img src={EditIcon} alt="edit" height="24px" /></Button></td>
-                <td><Button variant="light"><img src={DeleteIcon} alt="delete" height="24px" /></Button></td>
+                <td>
+                    <Button 
+                        variant="light" 
+                        onClick={() => this.handleEdit(grade.id)}>
+                            <img src={EditIcon} alt="edit" height="24px" />
+                    </Button>
+                </td>
+                <td>
+                    <Button 
+                        variant="light" 
+                        onClick={() => this.handleDelete(grade.id)}>
+                            <img src={DeleteIcon} alt="delete" height="24px" />
+                    </Button>
+                </td>
             </tr>
         ));
 
@@ -105,14 +127,17 @@ class GradesResults extends React.Component {
     }
 
     calcMin() {
+        if (this.props.grades.length == 0) return "";
         return this.props.grades.reduce((min, b) => Math.min(min, b.grade), this.props.grades[0].grade);
     }
 
     calcMax() {
+        if (this.props.grades.length == 0) return "";
         return this.props.grades.reduce((max, b) => Math.max(max, b.grade), this.props.grades[0].grade);
     }
 
     calcAvg() {
+        if (this.props.grades.length == 0) return "";
         return this.props.grades.reduce((a, b) => a + b.grade, 0) / this.props.grades.length;
     }
 
@@ -151,17 +176,44 @@ class App extends React.Component {
                 {id: 3, courseName: "English", grade: 40},
             ],
             editing: false,
+            editGrade: {id: -1, courseName: "Test", grade: 0},
         };
     }
 
-    handleGradeChange(type, courseName, grade) {
+    handleGradeChange(type, grade) {
         if (type == 'add') {
             let grades = this.state.grades;
-            grades.push({id: Math.random()*1000, courseName: courseName, grade: grade});
+            grades.push({id: Math.random()*1000, courseName: grade.courseName, grade: grade.grade});
             this.setState({grades: grades});
         } else if (type == 'edit') {
             // Edit
         }
+    }
+
+    handleEdit(id) {
+        let editGrade;
+
+        for (let i = 0; i < this.state.grades.length; i++) {
+            if (this.state.grades[i].id == id) {
+                editGrade = this.state.grades[i];
+                break;
+            }
+        }
+        
+        this.setState({editGrade: editGrade});
+        this.setState({editing: true});  
+    }
+
+    handleDelete(id) {
+        let grades = [];
+        
+        for (let i = 0; i < this.state.grades.length; i++) {
+            if (this.state.grades[i].id != id) {
+                grades.push(this.state.grades[i]);
+            }
+        }
+
+        this.setState({grades: grades});
     }
 
     render() {
@@ -172,10 +224,18 @@ class App extends React.Component {
                 </header>
                 <main className="container mt-5">
                     <div>
-                        <GradeForm editing={this.state.editing} onChange={this.handleGradeChange.bind(this)} />
+                        <GradeForm 
+                            editing={this.state.editing} 
+                            editGrade={this.state.editGrade}
+                            onChange={this.handleGradeChange.bind(this)} 
+                        />
                     </div>
                     <div className="mt-4">
-                        <GradesList grades={this.state.grades} />
+                        <GradesList 
+                            grades={this.state.grades} 
+                            onEdit={this.handleEdit.bind(this)}
+                            onDelete={this.handleDelete.bind(this)}
+                        />
                     </div>   
                     <div>
                         <GradesResults grades={this.state.grades} />
